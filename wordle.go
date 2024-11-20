8951b7f8a7b2551a1a2d7a51ba7a9ad46ecef270
Richard Gold <drshellface@outlook.com>
2024-11-20 16:21:30 +0000

    added test

--- wordle.go
+++ wordle.go
@@ -1,6 +1,10 @@
 package wordle
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/photonhq/wordle/words"
+)
 
 type wordleState struct {
 	// word is the word that the user is trying to guess
@@ -11,9 +15,38 @@ type wordleState struct {
 	currGuess int
 }
 
+// letterStatus can be none, correct, present, or absent
+type letterStatus int
+
+const (
+	// none = no status, not guessed yet
+	none letterStatus = iota
+	// absent = not in the word
+	absent
+	// present = in the word, but not in the correct position
+	present
+	// correct = in the correct position
+	correct
+)
+
+// newWordleState builds a new wordleState from a string.
+// Pass in the word you want the user to guess.
+func newWordleState(word string) wordleState {
+	var myWordle wordleState
+
+	for i := 0; i < len(word); i++ {
+		myWordle.word[i] = byte(word[i])
+	}
+	return myWordle
+}
+
 func wordle() {
 	fmt.Println("welcome to wordle!")
-	var myWorld wordleState
-	myWorld.currGuess = 1
-	fmt.Println(myWorld.currGuess)
+	newWord := words.GetWord()
+	myWordleState := newWordleState(newWord)
+	fmt.Println(myWordleState.word)
+}
+
+func main() {
+	wordle()
 }
--- wordle_test.go
+++ wordle_test.go
@@ -1,1 +1,21 @@
 package wordle
+
+import "testing"
+
+func TestNewWordleState(t *testing.T) {
+	word := "HELLO"
+	ws := newWordleState(word)
+	wordleAsString := string(ws.word[:])
+
+	for i := 0; i < len(word); i++ {
+		if !(word[i] == ws.word[i]) {
+			t.Logf(" lp0 on fire   ")
+
+		}
+	}
+
+	t.Log("Created wordleState:")
+	t.Logf("    word: %s", wordleAsString)
+	t.Logf("    guesses: %v", ws.guesses)
+	t.Logf("    currGuess: %v", ws.currGuess)
+}
