1c32b49f92c32dc93f950cb20c8af31a9b8309df
Richard Gold <drshellface@outlook.com>
2024-11-20 13:56:38 +0000

    added struct

--- wordle.go
+++ wordle.go
@@ -1,1 +1,19 @@
 package wordle
+
+import "fmt"
+
+type wordleState struct {
+	// word is the word that the user is trying to guess
+	word [5]byte
+	// guesses holds the guesses that the user has made
+	guesses [6]string
+	// currGuess is the index of the available slot in guesses
+	currGuess int
+}
+
+func wordle() {
+	fmt.Println("welcome to wordle!")
+	var myWorld wordleState
+	myWorld.currGuess = 1
+	fmt.Println(myWorld.currGuess)
+}
